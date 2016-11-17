471de313095ed0e391366717456a7728d664a359
Antonio García Maestre <antonio.garcia@ricston.com>
2016-11-17 13:25:24 +0100

    add Wish

---
+++ src/main/java/com/mycompany/myapp/repository/WishRepository.java
@@ -0,0 +1,15 @@
+package com.mycompany.myapp.repository;
+
+import com.mycompany.myapp.domain.Wish;
+
+import org.springframework.data.jpa.repository.*;
+
+import java.util.List;
+
+/**
+ * Spring Data JPA repository for the Wish entity.
+ */
+@SuppressWarnings("unused")
+public interface WishRepository extends JpaRepository<Wish,Long> {
+
+}
